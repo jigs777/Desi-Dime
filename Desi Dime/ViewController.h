989b16a30bf9f9b91e81eafc349e2f0198c248b8
hadwanihardik <iViprak1@iviprak1-bunti.local>
2016-01-27 17:41:27 +0530

    Initial Commit

---
+++ Desi Dime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Desi Dime
+//
+//  Created by iViprak1 on 27/01/16.
+//  Copyright © 2016 jigar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Desi Dime/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Desi Dime
+//
+//  Created by iViprak1 on 27/01/16.
+//  Copyright © 2016 jigar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
